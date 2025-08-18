ffb8bea7bcdd1bc54e331fe625fe6b54f3fb7749
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2025-08-18 16:24:12 +0300

    fix: open nvim-tree on startup

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,8 @@
 require("nvchad.autocmds")
+
+-- open nvim-tree on startup
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    callback = function()
+        require("nvim-tree.api").tree.open()
+    end,
+})

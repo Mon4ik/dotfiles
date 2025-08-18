c803e62d48c546b03544de1e64db677cf898d0bb
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2025-08-18 13:06:38 +0300

    feat: migrate old options

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,7 +1,3 @@
--- This file needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :(
-
 ---@type ChadrcConfig
 local M = {}
 
@@ -15,10 +11,6 @@ M.base46 = {
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.ui = {}
 
 return M
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,15 @@
 require "nvchad.options"
 
--- add yours here!
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
+opt.list = true
+opt.listchars = "space:·"
+
+o.shiftwidth = 2
+o.tabstop = 2
+o.softtabstop = 2

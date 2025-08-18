681a83bd16895f40a594d4faaa48e397074669d1
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2025-08-18 17:56:17 +0300

    fix: defer nvim-tree

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -3,6 +3,8 @@ require("nvchad.autocmds")
 -- open nvim-tree on startup
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
     callback = function()
-        require("nvim-tree.api").tree.open()
+        vim.schedule(function()
+            require("nvim-tree.api").tree.open()
+        end)
     end,
 })

828ee405dba376bf599366099640f345865f4836
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2025-08-19 19:58:30 +0300

    refactor: move lsp out

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = require("configs.lspconfig"),
+        config = function(_, opts)
+            dofile(vim.g.base46_cache .. "lsp")
+            require("nvchad.lsp").diagnostic_config()
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    for _, keymap in ipairs(opts.keymaps) do
+                        vim.keymap.set(
+                            keymap.mode or "n",
+                            keymap[1],
+                            keymap[2],
+                            { buffer = args.buf, desc = "LSP " .. keymap.desc }
+                        )
+                    end
+                end,
+            })
+
+            local baseConfig = require("nvchad.configs.lspconfig")
+            vim.lsp.config("*", { capabilities = baseConfig.capabilities, on_init = baseConfig.on_init })
+
+            local lspconfig = require("lspconfig")
+            for lsp, config in pairs(opts.servers) do
+                lspconfig[lsp].setup(vim.tbl_extend("keep", {
+                    capabilities = baseConfig.capabilities,
+                    on_init = baseConfig.on_init,
+                }, config))
+            end
+        end,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "onsails/lspkind.nvim",
+        },
+        opts = function()
+            return require("configs.nvim-cmp")
+        end,
+    },
+}
